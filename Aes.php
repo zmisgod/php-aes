75e8e4953fb68c856d5c7229bf3a7500f84f7690
zmigsod <tingping@TingPingdeMacBook-Pro-2.local>
2017-06-22 15:40:01 +0800

    new

--- Aes.php
+++ Aes.php
@@ -1,5 +1,10 @@
 <?php
-
+/**
+ * Aes is using PHP OpenSSL Extention
+ * The official of PHP manual sugest us using OpenSSL to replace Mcrypt extention
+ * When you are ready to use this class,
+ * You may check your OpenSSL extention is avaliable.
+ */
 class Aes
 {
     protected $iv;
@@ -11,25 +16,35 @@ class Aes
         $this->iv = base64_encode('qwertyuiopasdfgh');
     }
 
+    public function setKey($key)
+    {
+        $this->key = $key;
+    }
+
+    public function setIv($iv)
+    {
+        $this->iv = $iv;
+    }
+
     /**
-     * 加密
+     * encrypt
      *
      * @param $str
      * @return string
      */
-    public function encode($str)
+    public function encrypt($str)
     {
         $encrypted = openssl_encrypt($str, 'aes-256-cbc', base64_decode($this->key), OPENSSL_RAW_DATA, base64_decode($this->iv));
         return base64_encode($encrypted);
     }
 
     /**
-     * 解密
+     * decrypt
      *
      * @param $str
      * @return string
      */
-    public function decode($str)
+    public function decrypt($str)
     {
         $baseDecode = base64_decode($str);
         return openssl_decrypt($baseDecode, 'aes-256-cbc', base64_decode($this->key), OPENSSL_RAW_DATA, base64_decode($this->iv));
--- README.md
+++ README.md
@@ -1,14 +1,21 @@
 # php-aes
 > using OpenSSL extention
 
+This Aes Class is using OpenSSL Extention of PHP.
+[The official of PHP manual suggest us using OpenSSL to replace Mcrypt extention](http://php.net/manual/en/migration71.deprecated.php "Why am i using OpenSSL extention")
+When you are ready to use this class,
+You may check your OpenSSL extention is avaliable.
+
+##check
+* Check your php extention and find OpenSSL.
+`php -m | grep openssl` 
+* if you are not find, you'd better build this extention.
+
 ## useage
-* Check your php extention and find openssl
 * Copy this class into your code
 * Replace the `$this->key`(need 24 strings) and `$this->iv`(need 16 strings)
 <pre>
 $aes = new Aes();
-echo $en = $aes->encode('123');
-echo $de = $aes->edcode($en);
+echo $en = $aes->encrypt('123');
+echo $de = $aes->decrypt($en);
 </pre>
-
-##ok
